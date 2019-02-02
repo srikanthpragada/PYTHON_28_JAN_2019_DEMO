688595b3b119aec6e53049033a525550bb9c8b2f
srikanthpragada <srikanthpragada@gmail.com>
2019-02-02 07:59:32 +0530

    02-FEB

---
+++ ex/largest_number.py
@@ -0,0 +1,13 @@
+
+largest = 0
+while True:
+    num = int(input("Enter a number [0 to stop] :"))
+    if num == 0:
+        break
+
+    if num > largest:
+        largest = num
+
+print("Largest number = ", largest)
+
+
---
+++ ex/lowest_factor.py
@@ -0,0 +1,10 @@
+# Display lowest factor for the given number
+
+num = int(input("Enter a number :"))
+
+for i in range(2, num // 2 + 1):
+    if num % i == 0:
+        print(i)
+        break
+else:
+    print(num)
