c1b7a762e354f6f0123715ae011d686244cb4ad6
srikanthpragada <srikanthpragada@gmail.com>
2019-02-16 07:59:18 +0530

    16-FEB

--- oop/account.py
+++ oop/account.py
@@ -1,10 +1,37 @@
 class Account:
+    # Class attributes
+    minbal = 10000
+
+    @staticmethod
+    def set_minbal(newminbal):
+        Account.minbal = newminbal
+
     # Constructor
-    def __init__(self, acno, ahname):
+    def __init__(self, acno, ahname, balance=0):
         # Object attributes
-        self.acno = acno
+        self.acno = acno  # Private member
         self.ahname = ahname
-        self.balance = 0
+        self.balance = balance
+
+    # Methods
+    def deposit(self, amount):
+        self.balance += amount
+
+    def get_balance(self):
+        return self.balance
+
+    def __str__(self):
+        return f"{self.acno} - {self.ahname} - {self.balance}"
+
+    def __eq__(self, other):
+        print("__eq__")
+        return self.acno == other.acno
+
+    def __gt__(self, other):
+        return self.balance > other.balance
 
 
 a1 = Account(1, "Mr. Bill")
+Account.set_minbal(5000)
+
+a2 = Account(2, "Mr. Mark", 50000)
---
+++ oop/products.py
@@ -0,0 +1,41 @@
+class Product:
+    # class attribute
+    TAX = 18
+
+    def __init__(self, name, price, qoh=0):
+        self.name = name
+        self.price = price
+        self.qoh = qoh
+
+    def print_details(self):
+        print(self.name)
+        print(self.price)
+        print(self.qoh)
+
+    def get_net_price(self):
+        return self.price + (self.price * Product.TAX / 100)
+
+
+class DiscountProduct(Product):
+    def __init__(self, name, price, disrate, qoh=0):
+        super().__init__(name, price, qoh)
+        self.disrate = disrate
+
+    # Overriding
+    def print_details(self):
+        super().print_details()
+        print(self.disrate)
+
+    def get_net_price(self):
+        grossprice = self.price - (self.price * self.disrate / 100)
+        netprice = grossprice + (grossprice * Product.TAX / 100)
+        return netprice
+
+
+p = Product("Abc", 10000, 2)
+print(p.get_net_price())
+dp = DiscountProduct("Xyz", 15000, 20, 5)
+print(dp.get_net_price())
+
+p.print_details()
+dp.print_details()
