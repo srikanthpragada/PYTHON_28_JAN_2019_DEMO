5d834e8797c6d6b61e81bf8b1aafdf40b98ba09e
srikanthpragada <srikanthpragada@gmail.com>
2019-02-20 07:59:56 +0530

    20-FEB

--- oop/account.py
+++ oop/account.py
@@ -1,3 +1,8 @@
+class InsufficientBalanceError(Exception):
+    def __init__(self):
+        self.message = "Insufficient Balance!"
+
+
 class Account:
     # Class attributes
     minbal = 10000
@@ -17,6 +22,12 @@ class Account:
     def deposit(self, amount):
         self.balance += amount
 
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            raise InsufficientBalanceError()
+
     def get_balance(self):
         return self.balance
 
@@ -33,5 +44,9 @@ class Account:
 
 a1 = Account(1, "Mr. Bill")
 Account.set_minbal(5000)
-
 a2 = Account(2, "Mr. Mark", 50000)
+try:
+    a2.withdraw(100000)
+    print("Withdrew amount!")
+except InsufficientBalanceError as ex:
+    print("Error : ", ex.message)
